78be27fd6f64b28fc05b6cc6e7d526924c6f19e2
benyagmurz <benyagmurzzzz@gmail.com>
2024-12-10 16:03:31 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,18 +2,16 @@
 #Farklı fotograflar kullanıp gözlemledik.
 
 import cv2
+import numpy as np 
 
-im=cv2.imread('hand1.jpeg')
-gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-blurred=cv2.GaussianBlur(gray, (15,15),0)
-median=cv2.medianBlur(gray, 15)
-cv2.imwrite('original_image.jpg', im)       
-cv2.imwrite('gaussian_blur.jpg', blurred)
-cv2.imwrite('median_blur.jpg', median)  
-cv2.imshow('original image',im)
-cv2.imshow('Gaussian Blur',blurred)
-cv2.imshow('Median Blur',median)
+img=cv2.imread('hand1.jpeg')
+cv2.imshow('Origonal image' ,img)
+height, width = img.shape[:2]
+print(f"size of the image:{width}x{height}")
+pixel=img[100,100]
+print(f"Pixel value:{pixel}")
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # blur yapma denendi gaussian ve median blur çıkması ve orijinal fotografın da yanında çıması sağandı
+# imwrite kodu ile açtığımız ve blurlaştığmız xray fotograflarını kaydetmeyi sağladı
