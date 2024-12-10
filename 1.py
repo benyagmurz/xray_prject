a8bc917197860f0a9167b46db7f2c0d3ca79ccc6
benyagmurz <benyagmurzzzz@gmail.com>
2024-12-10 15:21:36 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+# Bu kodla istediğimiz xray fotograflarını kod kullanarak çağırdık.
+#Farklı fotograflar kullanıp gözlemledik.
+
+import cv2
+
+im=cv2.imread('knee.jpg')
+cv2.imshow('original image',im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
