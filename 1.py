d8a3728bed0d11a47f803ac16ceb6d9e7b90fc46
benyagmurz <benyagmurzzzz@gmail.com>
2024-12-10 15:41:36 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,6 +4,11 @@
 import cv2
 
 im=cv2.imread('hand1.jpeg')
+gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+blurred=cv2.GaussianBlur(gray, (15,15),0)
+median=cv2.medianBlur(gray, 15)
 cv2.imshow('original image',im)
+cv2.imshow('Gaussian Blur',blurred)
+cv2.imshow('Median Blur',median)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
